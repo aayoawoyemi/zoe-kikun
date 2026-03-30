7695ff8b472f62d3aefce489ac28a70d5f746d7f
aayoawoyemi <aayoawoyemi@gmail.com>
2026-03-30 14:21:28 -0500

    Fix PWA start_url for GitHub Pages subpath

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'zoe-kikun-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'zoe-kikun-v2';
+const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
